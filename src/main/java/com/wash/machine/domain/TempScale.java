4ec337f1353a25921bc4b2f3ee6cdf789d78884d
MartaM-a <martamorincewa@gmail.com>
2024-10-09 22:11:47 +0200

    changed tempScale logic

--- src/main/java/com/wash/machine/domain/TempScale.java
+++ src/main/java/com/wash/machine/domain/TempScale.java
@@ -2,33 +2,16 @@ package com.wash.machine.domain;
 
 public enum TempScale {
 
-    CELSIUS("\u2103") {
-        public double toScale(double value, TempScale scale) {
-            return scale == CELSIUS
-                    ? value
-                    : Math.round(value * 1, 8 + 32);
-        }
-    },
-        FAHRENHEIT("F") {
-            @Override
-        public double toScale(double value, TempScale scale) {
-                return scale == FAHRENHEIT ? value : Math.round( ((value - 32) /1.8);
-            }
-        };
+    CELSIUS("\u2103"), FAHRENHEIT("F");
 
     private final String symbol;
 
-    TempScale (String symbol) {
+    TempScale(String symbol) {
         this.symbol = symbol;
-
-        TempScale(String symbol) {
-            this.symbol = symbol;
-        }
-        @Override
-        public String toString() {
-            return symbol;
-        }
     }
+
+
 }
+
 
 
