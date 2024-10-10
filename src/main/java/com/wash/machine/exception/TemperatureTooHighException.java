2b8b682a8e770980ccc898a680bc6e0ccf7e0f2c
MartaM-a <martamorincewa@gmail.com>
2024-10-10 15:24:37 +0200

    added  Exceptions class

---
+++ src/main/java/com/wash/machine/exception/TemperatureTooHighException.java
@@ -0,0 +1,6 @@
+package com.wash.machine.exception;
+
+public class TemperatureTooHighException extends Exception{
+
+
+}
---
+++ src/main/java/com/wash/machine/exception/TemperatureTooLowExcception.java
@@ -0,0 +1,4 @@
+package com.wash.machine.exception;
+
+public class TemperatureTooLowExcception extends Exception{
+}
