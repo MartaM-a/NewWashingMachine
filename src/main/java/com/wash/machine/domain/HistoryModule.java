10d375373d53b75f9310b10ee2d24b1c40c76966
MartaM-a <martamorincewa@gmail.com>
2024-10-10 03:43:46 +0200

    added pogramDiscription

--- src/main/java/com/wash/machine/domain/HistoryModule.java
+++ src/main/java/com/wash/machine/domain/HistoryModule.java
@@ -1,14 +1,66 @@
 package com.wash.machine.domain;
 
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.ArrayList;
+
+import java.util.List;
+
 
 public class HistoryModule {
-    private LinkedList<Map<String, Object>> history;
-    private static final int MAX_HISTORY = 30;
+    public static class Log {
+        private final ProgramName name;
+        private final TempModule temp;
+        private final int v;
 
-    public HistoryModule() {
+        public Log(ProgramModule program, TempModule temp, int v) {
+            this.name = program.getName();
+            this.temp = temp;
+            this.v = v;
+        }
 
-        this.history = new LinkedList<>();
+        public ProgramName getName() {
+            return name;
+        }
+
+        public TempModule getTemp() {
+            return temp;
+        }
+
+        public int getV() {
+            return v;
+        }
+
+        @Override
+        public String toString() {
+            return "name=" + name
+                    +", temp=" + temp
+                    + ", v=" + v;
+        }
+    }
+
+    private static final int MAX_SIZE = 30;
+
+    private final List<Log> logs = new ArrayList<>();
+
+    public void add(Log log) {
+        if (this.logs.size() == MAX_SIZE) {
+            this.logs.remove(MAX_SIZE - 1);
+        }
+        this.logs.add(0, log);
+    }
+
+    public List<Log> getLogs() {
+        return logs;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        for (Log log : logs) {
+            b.append(log.toString())
+                    .append("\n");
+        }
+        return b.toString();
     }
 }
+
+
--- src/main/java/com/wash/machine/domain/WashingMachine.java
+++ src/main/java/com/wash/machine/domain/WashingMachine.java
@@ -6,22 +6,29 @@ public class WashingMachine {
     private final String name;
     private final WashingModule programs;
     private final ProgramModule currentProgram;
-    private int CurrentV;
+    private int currentV;
     private final HistoryModule history;
 
     public WashingMachine (String name) {
-        this(name,
-                new ProgramModule(   new WashingModule(), 100, 500, 30., 50., CELSIUS),
-                        new WashingModule(SYNTHETIC, 100, 500, 35., 50., CELSIUS),
-                        new WashingModule(DELICATE, 100, 500, 30., 40., CELSIUS),
-                        new WashingModule(INTENSIVE, 100, 500, 35., 60., CELSIUS),
-                        new WashingModule(QUICK, 100, 800, 30., 50., CELSIUS),
-                        new WashingModule(WOOL, 100, 600, 30., 50., CELSIUS),
-                        new WashingModule(SPORT, 100, 800, 30., 50., CELSIUS),
-                        new WashingModule(ECO, 100, 400, 30., 50., CELSIUS),
-                        new WashingModule(BABY, 100, 500, 40., 50., CELSIUS),
-                        new WashingModule(SUPERQUICK, 100, 800, 30., 50., CELSIUS)
-                );
+        this(
+                name,
+                new WashingModule(
+                        new ProgramModule(ProgramName.COTTON, 100,500,30, 50,TempScale.CELSIUS),
+                        new
+
+
+
+
+//                        new ProgramModule(SYNTHETIC, 100, 500, 35., 50., CELSIUS),
+//                        new ProgramModule(DELICATE, 100, 500, 30., 40., CELSIUS),
+//                        new ProgramModule(INTENSIVE, 100, 500, 35., 60., CELSIUS),
+//                        new ProgramModule(QUICK, 100, 800, 30., 50., CELSIUS),
+//                        new ProgramModule(WOOL, 100, 600, 30., 50., CELSIUS),
+//                        new ProgramModule(SPORT, 100, 800, 30., 50., CELSIUS),
+//                        new ProgramModule(ECO, 100, 400, 30., 50., CELSIUS),
+//                        new ProgramModule(BABY, 100, 500, 40., 50., CELSIUS),
+//                        new ProgramModule(SUPERQUICK, 100, 800, 30., 50., CELSIUS)
+                )
 
                 );
 
